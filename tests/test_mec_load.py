c0d8324e7523cfb7d622962423a33ab649765fca
Remis Lape <remis.lp@gmail.com>
2024-01-13 23:36:11 +0000

    more tests added. most still failing

---
+++ tests/test_mec_load.py
@@ -0,0 +1,47 @@
+from scalcs.samples import samples
+from scalcs import scalcsio
+from scalcs import scburst
+from scalcs import scalcslib as scl
+from scalcs import qmatlib as qml
+
+import os
+import sys
+import time
+import unittest
+import numpy as np
+
+
+class TestMecLoad(unittest.TestCase):
+
+    def setUp(self):
+        filename = './scalcs/samples/samples.xlsx'
+        os.path.isfile(filename)
+        self.mec = scalcsio.load_from_excel_sheet(filename, sheet=0)
+        self.conc = 100e-9 # 100 nM
+        self.mec.set_eff('c', self.conc)
+        self.tres = 0.0001 # 100 microsec
+        self.tcrit = 0.004
+
+    def test_load_from_excel(self):
+        assert len(self.mec.States) == 5
+        assert len(self.mec.Rates) == 10
+        assert len(self.mec.Cycles) == 1
+
+    def test_burst(self):
+        # # # Burst initial vector.
+        phiB = scburst.phiBurst(self.mec)
+        self.assertAlmostEqual(phiB[0], 0.275362, 6)
+        self.assertAlmostEqual(phiB[1], 0.724638, 6)
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestMecLoad)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+def test_mr():
+    filename = './scalcs/samples/samples.xlsx'
+    mec = scalcsio.load_from_excel_sheet(filename, sheet=1)
+    assert mec.Rates[8].rateconstants == 3.0
+    assert mec.Rates[10].rateconstants == 2.0
+    mec.update_mr()
+    assert mec.Rates[8].rateconstants == 1.0
+    assert mec.Rates[10].rateconstants == 1.0
---
+++ tests/test_popen.py
@@ -0,0 +1,43 @@
+import numpy.testing as npt
+
+from scalcs import popen
+from scalcs.samples import samples
+from scalcs import scalcslib as scl
+from scalcs import scplotlib as scpl
+
+
+def test_regression():
+    mec = samples.CH82()
+    tres = 0.0001 # 100 microsec
+    # POPEN CURVE CALCULATIONS
+    _, _, pi = scpl.Popen(mec, tres)
+    assert (pi[-1]>0.967 and pi[-1]<0.969)
+
+def test_import():
+    from scalcs.popen import PopenCurve
+    assert PopenCurve is not None
+
+def test_PopenCurve_constructor():
+    pc = popen.PopenCurve()
+    assert pc.tres == 0.0
+    assert pc.mec is None
+
+def test_pilot_curve():
+    
+    mec = samples.CH82()
+    tres = 0.0
+    pc = popen.PopenCurve(mec, tres)
+    
+    assert len(pc._pilot_conc) == 1000
+    assert len(pc._pilot_popen) == 1000
+    
+    npt.assert_almost_equal(pc.maxPopen, 0.9677419329868882, decimal=16)
+    npt.assert_almost_equal(pc.EC50, 2.403813848843297e-06, decimal=16)
+    npt.assert_almost_equal(pc.nH, 1.8920149941304116, decimal=16)
+
+
+    assert pc.is_not_decreasing
+    assert not pc.is_not_increasing
+    assert pc.is_monotonic
+    assert not pc.is_decreasing
+    assert pc.is_increasing
