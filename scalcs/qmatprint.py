8d9450e669890f64dd7b544b149d39180cf12fe4
Remis <remis.lp@gmail.com>
2024-09-07 15:07:47 +0100

    print() removed from printout classes

--- scalcs/qmatprint.py
+++ scalcs/qmatprint.py
@@ -14,6 +14,7 @@ class QMatrixPrints(QMatrix):
         # Initialize the QMatrix instance.
         QMatrix.__init__(self, Q, kA=kA, kB=kB, kC=kC, kD=kD)
 
+    @property
     def print_Q(self):
         q_mat_str = '\nQ (units [1/s]) = \n'
         q_mat_str += tabulate((['{:.3f}\t'.format(item) for item in row]
@@ -21,19 +22,25 @@ class QMatrixPrints(QMatrix):
                               headers=[i+1 for i in range(self.k)], 
                               showindex=[i+1 for i in range(self.k)],
                               tablefmt='orgtbl')       
-        print(q_mat_str)
+        return q_mat_str
 
+    @property
     def print_pinf(self):
-        print('\nEquilibrium state occupancies:')
-        print(tabulate([self.pinf], headers=[i+1 for i in range(self.k)], tablefmt='orgtbl' ))
+        pinf_str = '\nEquilibrium state occupancies:'
+        pinf_str += tabulate([self.pinf], headers=[i+1 for i in range(self.k)], tablefmt='orgtbl' )
+        return pinf_str
 
+    @property
     def print_Popen(self):
-        print('\nEquilibrium open probability Popen = {0:.6f}'.format(self.Popen()))
+        return '\nEquilibrium open probability Popen = {0:.6f}'.format(self.Popen())
 
+    @property
     def print_state_lifetimes(self):
-        print('\nMean lifetimes of each individual state (ms):')
-        print(tabulate([1000 / self.state_lifetimes()], headers=[i+1 for i in range(self.k)], tablefmt='orgtbl' ))
+        lifetimes_str = '\nMean lifetimes of each individual state (ms):'
+        lifetimes_str += tabulate([1000 / self.state_lifetimes()], headers=[i+1 for i in range(self.k)], tablefmt='orgtbl' )
+        return lifetimes_str
 
+    @property
     def print_transition_matrices(self):
         """
         Print the transition probabilities and frequencies.
@@ -52,8 +59,9 @@ class QMatrixPrints(QMatrix):
                               headers=[i+1 for i in range(self.k)], 
                               showindex=[i+1 for i in range(self.k)],
                               tablefmt='orgtbl')
-        print(info_str)
+        return info_str
 
+    @property
     def print_subset_probabilities(self):
 
         info_str = '\nInitial probabilities of finding channel in classes of states:'
@@ -66,15 +74,17 @@ class QMatrixPrints(QMatrix):
         info_str += '\nP1(B|F)= {:.4g}'.format(self.P('B|F')) + '\t\tOf channels that a shut (in F) at t=0, probability being in B'
         info_str += '\nP1(C|F)= {:.4g}'.format(self.P('C|F')) + '\t\tOf channels that a shut (in F) at t=0, probability being in C'
 
-        print(info_str)
+        return info_str
 
+    @property
     def print_initial_vectors(self):
         info_str = '\nInitial vectors (conditional probability distribution over a subset of states):'
         info_str += '\nphi(A)= {}'.format(self.phi('A'))
         info_str += '\nphi(B)= {}'.format(self.phi('B'))
         info_str += '\nphi(F)= {}'.format(self.phi('F'))
-        print(info_str)
+        return info_str
 
+    @property
     def print_DC_table(self):
         """
         """
@@ -83,7 +93,7 @@ class QMatrixPrints(QMatrix):
         for i in range(1, self.k+1):
             mean_latencies.append(self.mean_latency_given_start_state(i))
 
-        print('\n') 
+        dc_str = '\n'
         header_open = ['Open \nstates', 'Equilibrium\n occupancy', 'Mean lifetime\n (ms)',
                        'Mean latency (ms)\nto next shutting\ngiven start \nin this state']
         DCtable_open = []
@@ -93,7 +103,7 @@ class QMatrixPrints(QMatrix):
                 DCtable_open.append(['Subset A ', sum(self.pinf[:self.kA]), mean_life_A * 1000, ' '])
             DCtable_open.append([i+1, self.pinf[i], 1000*self.state_lifetimes()[i], 1000*mean_latencies[i]])
 
-        print(tabulate(DCtable_open, headers=header_open, tablefmt='orgtbl', floatfmt=".6f"), '\n\n')
+        dc_str += (tabulate(DCtable_open, headers=header_open, tablefmt='orgtbl', floatfmt=".6f") + '\n\n')
 
         header_shut = ['Shut\nstates', 'Equilibrium\n occupancy', 'Mean lifetime\n (ms)',
                        'Mean latency (ms)\nto next opening\ngiven start \nin this state']
@@ -110,8 +120,8 @@ class QMatrixPrints(QMatrix):
                 DCtable_shut.append(['Subset D ', sum(self.pinf[self.kG: self.k]), mean_life_D * 1000, '-'])
             DCtable_shut.append([i+1, self.pinf[i], 1000*self.state_lifetimes()[i], 1000*mean_latencies[i]])
 
-        print(tabulate(DCtable_shut, headers=header_shut, tablefmt='orgtbl', floatfmt=(None, '.6f', '.6g', '.6g',)))
-    
+        dc_str += tabulate(DCtable_shut, headers=header_shut, tablefmt='orgtbl', floatfmt=(None, '.6f', '.6g', '.6g',))
+        return dc_str
 
 class SCBurstPrints(SCBurst):
     '''
@@ -121,56 +131,81 @@ class SCBurstPrints(SCBurst):
     def __init__(self, Q, kA=1, kB=1, kC=0, kD=0):
         SCBurst.__init__(self, Q, kA=kA, kB=kB, kC=kC, kD=kD)
 
+    @property
+    def print_all(self):
+        """
+        Output burst calculations.
+        """
+
+        all_str = ('\n*******************************************\n' +
+        'CALCULATED SINGLE CHANNEL BURST MEANS, PDFS, ETC....\n')
+
+        all_str += self.print_start_end_vectors
+        all_str += self.print_popen
+        all_str += self.print_length_pdf
+        all_str += self.print_openings_pdf
+        all_str += self.print_shuttings_pdf
+        return all_str
+
+    @property
     def print_start_end_vectors(self):
 
-        print('\nBurst start vector = ')
-        print(self.start_burst())
-        print('\nBurst end vector =')
-        print(self.end_burst())
+        info_str = '\nBurst start vector = '
+        for i in range(self.kA):
+            info_str += '\t{0:.5g}\t'.format(self.start_burst[i])
+        info_str += '\nBurst end vector ='
+        for i in range(self.kA):
+            info_str += '\t{0:.5g}\t'.format(self.end_burst[i, 0])
+        return info_str
 
+    @property
     def print_means(self):
         
-        print('\nMean number of opening per burst = {0:.6g}'.format(self.mean_number_of_openings()))
+        info_str = '\n\nMean number of opening per burst = {0:.6g}'.format(self.mean_number_of_openings)
 #        print('\nNo of gaps within burst per unit open time = {0:.6g} \n'.
 #              format((self.mean_number_of_openings() - 1) / self.mean_open_time()))
-        print('Mean burst length (ms)= {0:.6g}'.format(1000 * self.mean_length()))
-        print('Mean open time per burst (ms)= {0:.6g}'.format(1000 * self.mean_open_time()))
-        print('Mean shut time per burst (ms; all bursts)= {0:.6g}'.format(1000 * self.mean_shut_time()))
-        print('Mean shut time per burst (ms; excluding single opening bursts)= {0:.6g}'.
-            format(1000 * self.mean_shut_time() / self.probability_more_than_one_opening()))
-        print('Mean shut time between bursts (ms)= {0:.6g}'.format(1000 * self.mean_shut_times_between_bursts()))
+        info_str += '\nMean burst length (ms)= {0:.6g}'.format(1000 * self.mean_length)
+        info_str += '\nMean open time per burst (ms)= {0:.6g}'.format(1000 * self.mean_open_time)
+        info_str += '\nMean shut time per burst (ms; all bursts)= {0:.6g}'.format(1000 * self.mean_shut_time)
+        info_str += ('\nMean shut time per burst (ms; excluding single opening bursts)= {0:.6g}'.
+            format(1000 * self.mean_shut_time / self.probability_more_than_one_opening))
+        info_str += '\nMean shut time between bursts (ms)= {0:.6g}'.format(1000 * self.mean_shut_times_between_bursts)
+        return info_str
+ 
+    @property
+    def print_popen(self):
+        
+        info_str = '\n\nPopen WITHIN BURST = (open time/burst)/(bst length) = {0:.5g}'.format(self.burst_popen)
+        info_str += ('\nTotal Popen = (open time/bst)/(bst_length + mean gap between burst) = {0:.5g}'.format(self.total_popen))
+        return info_str
 
-        print('\nPopen WITHIN BURST = (open time/burst)/(bst length) = {0:.5g} \n'.format(self.mean_open_time() / self.mean_length()))
-        print('Total Popen = (open time/bst)/(bst_length + mean gap between burst) = {0:.5g} \n'.
-              format(self.mean_open_time() / (self.mean_length() + self.mean_shut_times_between_bursts())))
-
+    @property
     def print_length_pdf(self):
         e1, w1 = self.length_pdf_components()
-        print(expPDF_printout(e1, w1, 'PDF of total burst length, unconditional'))
-        
+        info_str = (expPDF_printout(e1, w1, '\nPDF of total burst length, unconditional'))
         e2, w2 = self.length_pdf_no_single_openings_components()
-        print(expPDF_printout(e2, w2, 'PDF of burst length for bursts with 2 or more openings'))
-
+        info_str += expPDF_printout(e2, w2, '\nPDF of burst length for bursts with 2 or more openings')
+        return info_str
+ 
+    @property
     def print_openings_pdf(self):
         e1, w1 = self.total_open_time_pdf_components()
-        print(expPDF_printout(e1, w1, 'PDF of total open time per burst'))
-
+        info_str = expPDF_printout(e1, w1, '\nPDF of total open time per burst')
         e2, w2 = self.first_opening_length_pdf_components()
-        print(expPDF_printout(e2, w2, 'PDF of first opening in a burst with 2 or more openings'))
-
+        info_str += (expPDF_printout(e2, w2, '\nPDF of first opening in a burst with 2 or more openings'))
         rho, w = self.openings_distr_components()
-        print(geometricPDF_printout(rho, w, 'Geometric PDF of of number (r) of openings / burst (unconditional)'))
+        info_str += (geometricPDF_printout(rho, w, '\nGeometric PDF of of number (r) of openings / burst (unconditional)'))
+        return info_str
 
+    @property
     def print_shuttings_pdf(self):
-
         e1, w1 = self.shut_times_inside_burst_pdf_components()
-        print(expPDF_printout(e1, w1, 'PDF of gaps inside burst'))
-
+        info_str = (expPDF_printout(e1, w1, '\nPDF of gaps inside burst'))
         e2, w2 = self.shut_times_between_burst_pdf_components()
-        print(expPDF_printout(e2, w2, 'PDF of gaps between burst'))
-
+        info_str += (expPDF_printout(e2, w2, '\nPDF of gaps between burst'))
         e3, w3 = self.shut_time_total_pdf_components_2more_openings()
-        print(expPDF_printout(e3, w3, 'PDF of total shut time per bursts for bursts with at least 2 openings'))
+        info_str += (expPDF_printout(e3, w3, '\nPDF of total shut time per bursts for bursts with at least 2 openings'))
+        return info_str
 
 def expPDF_printout(eigs, amps, title):
     """
